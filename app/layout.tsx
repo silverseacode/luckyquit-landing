c133764e89728151eb8436733aaf9248575ec412
Franco Coronel <franconicolascoronel94@gmail.com>
2023-09-06 11:10:39 -0300

    changed document title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Montserrat } from 'next/font/google'
 const montserrat = Montserrat({ subsets: ['latin'],weight: ["500", "700"], variable:"--font-montserrat" })
 
 export const metadata = {
-  title: "Lucky Quit | Your Path to Quit Smoking Forever | Smoking Cessation App",
+  title: "Lucky Quit | Quit Smoking App",
   description: "Discover top-notch smoking cessation coaches and take control of your journey to quit smoking. Our app empowers you to track your cigarette consumption, monitor your health progress, and visualize the money saved. Start your smoke-free life today!",
-  keywords: ["quit smoking","Smoke-free app", "Nicotine addiction help", "Motivational support for quitting smoking","Quit smoking benefits"]
+  keywords: ["quit smoking app","Smoke-free app", "Nicotine addiction help", "Motivational support for quitting smoking","Quit smoking benefits"]
 };
 
 export default function RootLayout({
