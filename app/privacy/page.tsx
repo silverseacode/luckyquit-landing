ff156f12a5b5b066998a34d17a59e00cedb499c3
Franco Coronel <franconicolascoronel94@gmail.com>
2023-09-19 14:11:35 -0300

    privacy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,87 @@
+export default function Privacy() {
+    return(
+        <div style={{display: "flex" ,justifyContent: "center", flexDirection: "column", padding: 20}}>
+              <h2>Privacy Policy</h2>
+
+              <p>
+                Thank you for using the Quit Smoking App (Lucky Quit). This
+                Privacy Policy outlines how we collect, use, and protect your
+                personal information. By using the App or the web, you agree to
+                the terms of this Privacy Policy.
+              </p>
+
+              <p>Information We Collect:</p>
+              <p>
+                1.1. Personal Information: When you use the App or the web, we
+                may collect certain personal information such as your name,
+                email address, and payment details (handled securely by PayPal).
+              </p>
+              <p>
+                1.2. Usage Information: We may collect information about your
+                use of the App or the web, including your interactions,
+                preferences, and activities.
+              </p>
+
+              <p>Use of Personal Information:</p>
+              <p>2.1. We use your personal information to:</p>
+              <p>- Facilitate the payment process between users and coaches.</p>
+              <p>
+                - Provide and improve the App's or web functionality and
+                services.
+              </p>
+              <p>
+                - Respond to your inquiries, requests, or customer support
+                needs.
+              </p>
+              <p>
+                - Analyze and monitor usage patterns to enhance user experience.
+              </p>
+              <p>
+                2.2. We will not sell, rent, or disclose your personal
+                information to third parties without your consent, except as
+                required by law.
+              </p>
+
+              <p>Data Security:</p>
+              <p>
+                3.1. We implement industry-standard security measures to protect
+                your personal information from unauthorized access, alteration,
+                or disclosure.
+              </p>
+              <p>
+                3.2. However, please note that no method of transmission over
+                the internet or electronic storage is 100% secure. Therefore, we
+                cannot guarantee absolute security of your information.
+              </p>
+
+              <p>Third-Party Services:</p>
+              <p>
+                4.1. The App or web may include links to third-party websites or
+                services. These external sites are not governed by this Privacy
+                Policy, and we are not responsible for their privacy practices.
+                We encourage you to review the privacy policies of those
+                third-party sites.
+              </p>
+
+              <p>Changes to Privacy Policy:</p>
+              <p>
+                5.1. We may update this Privacy Policy from time to time. Any
+                changes will be posted on this page, and the effective date will
+                be indicated at the top of the policy.
+              </p>
+
+              <p>Contact Us:</p>
+              <p>
+                6.1. If you have any questions, concerns, or requests regarding
+                this Privacy Policy or your personal information, please contact
+                us at lucky.quit.app@gmail.com.
+              </p>
+
+              <p>
+                By using the Quit Smoking App or web, you consent to the
+                collection, use, and processing of your personal information as
+                described in this Privacy Policy.
+              </p>
+            </div>
+    )
+}
