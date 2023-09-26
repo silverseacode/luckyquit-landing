733263c471f048a09657d12b3a5bfb11b47764a8
Franco Coronel <franconicolascoronel94@gmail.com>
2023-09-26 19:02:58 -0300

    terms page

---
+++ app/terms/page.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function Terms() {
+  return (
+    <div style={{display: "flex" ,justifyContent: "center", flexDirection: "column", padding: 20}}>
+      <h2>Terms of Service</h2>
+
+      <p>
+        {" "}
+        Welcome to the Quit Smoking App ("Lucky Quit"). By using the App or the
+        web, you agree to the following terms and conditions:
+      </p>
+
+      <p>Payment Process:</p>
+      <p>
+        {" "}
+        1.1. Users of the App or the web can make one-time payments to coaches
+        for their services through PayPal.
+      </p>
+      <p>
+        1.2. The payment amount and duration of the coaching plan are determined
+        by the coach.
+      </p>
+      <p>
+        1.3. Lucky Quit (the App) receives a 10% commission from each payment
+        made to the coach.
+      </p>
+      <p>
+        1.4. PayPal's terms and conditions apply to the payment process and any
+        disputes related to payments should be resolved with PayPal directly.
+      </p>
+
+      <p>Coach Responsibilities:</p>
+      <p>
+        2.1. Coaches are responsible for setting the price and duration of their
+        coaching plans.
+      </p>
+      <p>
+        2.2. The coach is solely responsible for delivering the coaching
+        services as agreed upon with the user.
+      </p>
+      <p>
+        2.3. Lucky Quit (the App) does not guarantee the quality or
+        effectiveness of the coaching services provided by the coach.
+      </p>
+      <p>
+        2.4. If the coach fails to respect the agreed duration of the coaching
+        plan or does not fulfill their obligations, the user should address the
+        issue directly with the coach.
+      </p>
+
+      <p>Lucky Quit's Role:</p>
+      <p>
+        3.1. Lucky Quit (the App) acts as a platform to connect users with
+        coaches and facilitate the payment process.
+      </p>
+      <p>
+        3.2. Lucky Quit does not endorse or verify the qualifications or
+        credentials of coaches.
+      </p>
+      <p>
+        3.3. Lucky Quit is not responsible for any disputes, claims, or damages
+        arising from the coaching services provided by the coach.
+      </p>
+      <p>
+        3.4. Any issues or concerns related to the coaching services should be
+        resolved directly between the user and the coach.
+      </p>
+
+      <p>Disclaimer:</p>
+      <p>
+        4.1. The information and content provided within the App or web are for
+        educational and informational purposes only. They are not a substitute
+        for professional medical advice or treatment.
+      </p>
+      <p>
+        4.2. Users should consult with a healthcare professional before making
+        any changes to their smoking habits or engaging in the coaching services
+        offered through the App or web.
+      </p>
+    </div>
+  );
+}
